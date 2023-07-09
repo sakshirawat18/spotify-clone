7b9fe560e11033ca711f9a84f9d7d82ecfbec446
Sakshi Rawat <sakshirawat@Sakshis-MacBook-Air.local>
2023-07-09 18:50:31 +0530

    updated create fn and displayed your playlist

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -4,7 +4,7 @@ import { songs } from './PlayControls';
 const Playlists = () => {
   const [showDropdown, setShowDropdown] = useState(false);    //keeps track of whether the dropdown with song options should be shown or hidden.
   const [selectedSongs, setSelectedSongs] = useState([]);   //an array that stores the IDs of the selected songs.
-  const [playlist, setPlaylist] = useState([])
+  const [playlist, setPlaylist] = useState([])  // Holds the selected songs as a playlist.
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
@@ -13,15 +13,19 @@ const Playlists = () => {
   // It adds or removes the song ID from the selectedSongs array based on whether it's already present or not.
   const handleSongSelection = (songId) => {
     if (selectedSongs.includes(songId)) {
-      setSelectedSongs(selectedSongs.filter((id) => id !== songId));
+      setSelectedSongs(selectedSongs.filter((id) => id !== songId));  // Deselect the song if it's already selected.
     } else {
-      setSelectedSongs([...selectedSongs, songId]);
+      setSelectedSongs([...selectedSongs, songId]);   // Select the song if it's not already selected.
     }
   };
 
   const create = () => {
-    
+    const selectedSongsArray = selectedSongs.map((songId) =>
+      songs.find((song) => song.id === songId)
+    );
+    setPlaylist(selectedSongsArray);    // Update the playlist state with the selected songs.
   };
+
 
   return (
     <div>
@@ -33,19 +37,23 @@ const Playlists = () => {
           <ul>
             {songs.map((song) => (
               <li key={song.id}>
-                  <input
-                    type="checkbox"
-                    checked={selectedSongs.includes(song.id)}
-                    onChange={() => handleSongSelection(song.id)}
-                  />
-                  {song.name}
+                <input
+                  type="checkbox"
+                  checked={selectedSongs.includes(song.id)}
+                  onChange={() => handleSongSelection(song.id)}
+                />
+                {song.name}
               </li>
             ))}
           </ul>
           <button onClick={create}>Create</button>
 
           <h3>Your Playlist:</h3>
-          {selectedSongs}
+          <ul>
+            {playlist.map((song) => (
+              <li key={song.id}>{song.name}</li>  // Renders each selected song as a list item with a unique key based on the song's id and displays the song's name.
+            ))}
+          </ul>
         </div>
       )}
     </div>
